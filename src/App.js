7b91ea963194bc36e5b43ab5ea26c12f6776ae7d
Akshayknchalapalli <akshaysmart31@gmail.com>
2023-11-14 16:13:02 +0530

    Rendering data from Array ,keys importance in maps

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
+import Text from "./Text";
 const App = () => {
-  const handleClickAction = () => {
-    console.log("I was clicked from parent");
+  const [data, setData] = useState([
+    { id: "a", text: "text 0" },
+    { id: "b", text: "text 1" },
+    { id: "c", text: "text 2" },
+    { id: "d", text: "text 3" },
+    { id: "e", text: "text 4" },
+    { id: "f", text: "text 5" },
+  ]);
+
+  const addMoreData = () => {
+    setData((prevData) => [{ id: "g", text: "text 6" }, ...prevData]);
   };
   return (
-    <Button data={{ a: { s: { d: "c" } } }} onClick={handleClickAction}>
-      Click Me
-    </Button>
+    <>
+      {data.map((item, index) => (
+        <Text key={item.id}>{item.text}</Text>
+      ))}
+      <Button clickAction={addMoreData}>Add more data</Button>
+    </>
   );
 };
 
